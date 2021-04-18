b15f7fc4f6c358312c737f42d776361c1023b7e9
Ubuntu <ubuntu@ip-172-31-19-2.eu-west-3.compute.internal>
2021-04-18 13:03:54 +0000

    Improvements hehe :)

--- zsh/link.sh
+++ zsh/link.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+git clone https://github.com/brymck/print-alias.git
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/dotfiles/zsh/oh-my-zsh
+
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
