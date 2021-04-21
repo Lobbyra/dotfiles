c4131455091bbb13895f15081ae2aac3b7634b19
lobbyra <jecaudal@student.42.fr>
2021-04-21 16:35:07 +0200

    add pluggin cloning in zsh link script

--- zsh/link.sh
+++ zsh/link.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/brymck/print-alias.git
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/dotfiles/zsh/oh-my-zsh
+rm -rf $HOME/dotfiles/zsh/print-alias
+git clone https://github.com/brymck/print-alias.git \
+          $HOME/dotfiles/zsh/print-alias
+
+rm -rf $HOME/dotfiles/zsh/oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git \
+          $HOME/dotfiles/zsh/oh-my-zsh
 
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
