06449a3e7c03b1b9edcf15b4428ba89a8eb6aeb0
Jérémy <jecaudal@student.42.fr>
2021-05-12 20:08:22 +0200

    update

--- tmux/link.sh
+++ tmux/link.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/tmux/ ~/.tmux
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+rm -rf $HOME/dotfiles/tmux/plugins/tpm/
+git clone https://github.com/tmux-plugins/tpm.git \
+          $HOME/dotfiles/tmux/plugins/tpm/
+
+rm -rf $HOME/dotfiles/tmux/plugins/tmux-themepack/
+git clone https://github.com/jimeh/tmux-themepack.git \
+          $HOME/dotfiles/tmux/plugins/tmux-themepack/
+
+rm -rf $HOME/dotfiles/tmux/plugins/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-sensible.git \
+          $HOME/dotfiles/tmux/plugins/tmux-sensible/
+
+ln -sf ~/dotfiles/tmux/ ~/.tmux
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
